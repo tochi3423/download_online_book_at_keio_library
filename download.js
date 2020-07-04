36c45f1cc24efd9662bb1c3f3ec1b5e98b909fa9
souta3423 <souta3423@gmail.com>
2020-07-04 10:03:44 +0900

    download_online_book

---
+++ download.js
@@ -0,0 +1,25 @@
+function downloadFromUrlAutomatically(url, fileName) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'blob';
+  xhr.onload = function (e) {
+    if (this.status == 200) {
+      var urlUtil = window.URL || window.webkitURL;
+      var imgUrl = urlUtil.createObjectURL(this.response);
+      var link = document.createElement('a');
+      link.href = imgUrl;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link)
+    }
+  };
+  xhr.send();
+}
+
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+for (var i = 0; i < 375; i++) {
+  gotoNextPageAction();
+  await _sleep(10000);
+  downloadFromUrlAutomatically(document.querySelector("#pagepict").getAttribute("src"), `image${i}.png`)
+}
